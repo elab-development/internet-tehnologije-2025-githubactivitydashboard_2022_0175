1727dac945f6f6403b9038e8c695c5db763c4a95
Anja Stanisic <anjasstanisic@gmail.com>
2026-01-29 12:33:12 +0100

    Initial commit: Basic Flask setup with JSON response

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "status": "uspeh",
+        "poruka": "GitHub Dashboard API radi!"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
